98502d053d1228fce95e8750d60b64c0a27f90e0
Andi Shafira Dyah K. <48061292+Andishafira@users.noreply.github.com>
2025-09-20 11:51:27 +0700

    test update 2

--- Belajar_Penerapan_Data_Science/Tugas_2_Menyelesaikan_Permasalahan_Institusi_Pendidikan/preprocessing.py
+++ Belajar_Penerapan_Data_Science/Tugas_2_Menyelesaikan_Permasalahan_Institusi_Pendidikan/preprocessing.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import joblib
 
-encoder_Application_mode = joblib.load(f"model/encoder_Application_mode.joblib")
+encoder_Application_mode = joblib.load("./Belajar_Penerapan_Data_Science/Tugas_2_Menyelesaikan_Permasalahan_Institusi_Pendidikan/model/encoder_Application_mode.joblib")
 encoder_Debtor = joblib.load("model/encoder_Debtor.joblib")
 encoder_Gender = joblib.load("model/encoder_Gender.joblib")
 encoder_Scholarship_holder = joblib.load("model/encoder_Scholarship_holder.joblib")
