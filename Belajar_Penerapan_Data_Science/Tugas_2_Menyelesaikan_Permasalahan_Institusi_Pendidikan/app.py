b1e7716e6bf7c8b28619702464664cc672ae60cf
Andi Shafira Dyah K. <48061292+Andishafira@users.noreply.github.com>
2025-09-20 12:13:59 +0700

    Mengubah tampilan UI

--- Belajar_Penerapan_Data_Science/Tugas_2_Menyelesaikan_Permasalahan_Institusi_Pendidikan/app.py
+++ Belajar_Penerapan_Data_Science/Tugas_2_Menyelesaikan_Permasalahan_Institusi_Pendidikan/app.py
@@ -9,52 +9,53 @@ st.header('Student Performance Analysis')
 
 data = pd.DataFrame()
 
-col1, col2, col3, col4, col5 = st.columns(5)
+col1, col2, col3 = st.columns(3)
 
 with col1:
     Application_mode = st.selectbox('Application mode', encoder_Application_mode.classes_, index=1)
     data['Application_mode'] = [Application_mode]
 
 with col2:
-    Debtor = st.selectbox('Debtor', encoder_Debtor.classes_, index=1)
-    data['Debtor'] = [Debtor]
-
-with col3:
-    Tuition_fees_up_to_date = st.selectbox('Tuition fees up to date', encoder_Tuition_fees_up_to_date.classes_, index=1)
-    data['Tuition_fees_up_to_date'] = [Tuition_fees_up_to_date]
-
-with col4:
     Gender = st.selectbox('Gender', encoder_Gender.classes_, index=1)
     data['Gender'] = [Gender]
 
-with col5:
-    Scholarship_holder = st.selectbox('Scholarship holder', encoder_Scholarship_holder.classes_, index=1)
-    data['Scholarship_holder'] = [Scholarship_holder]
-
-col1, col2, col3, col4, col5 = st.columns(5)
-
-with col1:
+with col3:
     Age_at_enrollment = st.number_input('Age at enrollment', min_value=15, max_value=70, value=15)
     data['Age_at_enrollment'] = [Age_at_enrollment]
 
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    Debtor = st.selectbox('Debtor', encoder_Debtor.classes_, index=1)
+    data['Debtor'] = [Debtor]
+
 with col2:
+    Tuition_fees_up_to_date = st.selectbox('Tuition fees up to date', encoder_Tuition_fees_up_to_date.classes_, index=1)
+    data['Tuition_fees_up_to_date'] = [Tuition_fees_up_to_date]
+
+with col3:
+    Scholarship_holder = st.selectbox('Scholarship holder', encoder_Scholarship_holder.classes_, index=1)
+    data['Scholarship_holder'] = [Scholarship_holder]
+
+col1, col2, col3, col4 = st.columns(4)
+
+with col1:
     Curricular_units_1st_sem_approved = st.number_input('Curricular units 1st sem approved', min_value=0, max_value=60, value=0)
     data['Curricular_units_1st_sem_approved'] = [Curricular_units_1st_sem_approved]
 
-with col3:
+with col2:
     Curricular_units_1st_sem_grade = st.number_input('Curricular units 1st sem grade', min_value=0, max_value=60, value=0)
     data['Curricular_units_1st_sem_grade'] = [Curricular_units_1st_sem_grade]
 
-with col4:
+with col3:
     Curricular_units_2nd_sem_approved = st.number_input('Curricular units 2nd sem approved', min_value=0, max_value=60, value=0)
     data['Curricular_units_2nd_sem_approved'] = [Curricular_units_2nd_sem_approved]
 
-with col5:
+with col4:
     Curricular_units_2nd_sem_grade = st.number_input('Curricular units 2nd sem grade', min_value=0, max_value=60, value=0)
     data['Curricular_units_2nd_sem_grade'] = [Curricular_units_2nd_sem_grade]
 
 if st.button('Predict'):
     clean_data = data_preprocessing(data=data)
     result = prediction(clean_data)
-    st.write("Results:")
-    st.write(result)
+    st.write("Results: " + result)
